b5ed5de67f17faf02e719473df167e431555ecc3
Mojaman <141308466+Mojaman@users.noreply.github.com>
2026-02-06 14:05:45 +0900

    Add Firebase configuration to main.js
    Added Firebase configuration object to main.js

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCJ8I6EclhXq-qa9ypJbYnEVprqkGUQ1rg",
+  authDomain: "api-test-e6972.firebaseapp.com",
+  projectId: "api-test-e6972",
+  storageBucket: "api-test-e6972.firebasestorage.app",
+  messagingSenderId: "169059356277",
+  appId: "1:169059356277:web:b98c5060eb1cee1a22323c",
+  measurementId: "G-3KS0SSQ5K4"
+};
+
 // Firebaseの機能を「外部から読み込む」
 import { initializeApp } from
   "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
